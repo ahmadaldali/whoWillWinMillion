9b2f4f011a12b47aa782f0bd12e05ab1bff43e11
Ahmad Aldali <ahmad@zeuor.com>
2021-02-20 17:00:11 +0200

    add icons instead of images

--- lib/components/help.dart
+++ lib/components/help.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // ignore: must_be_immutable
 class HelpEclipse extends StatelessWidget {
@@ -8,18 +9,25 @@ class HelpEclipse extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.025,
-              vertical: MediaQuery.of(context).size.height * 0.02),
-          width: MediaQuery.of(context).size.width * 0.25,
-          height: MediaQuery.of(context).size.width * 0.15,
-          // color: Colors.pink,
-          decoration: BoxDecoration(
-            color: Colors.blue[700],
-            border: Border.all(color: Colors.white, width: 2),
-            borderRadius: new BorderRadius.all(Radius.elliptical(100, 50)),
-          ),
-          child: Image.asset('assets/' + ty + '.png')),
+        margin: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.025,
+            vertical: MediaQuery.of(context).size.height * 0.02),
+        width: MediaQuery.of(context).size.width * 0.25,
+        height: MediaQuery.of(context).size.width * 0.15,
+        // color: Colors.pink,
+        decoration: BoxDecoration(
+          color: Colors.blue[700],
+          border: Border.all(color: Colors.white, width: 2),
+          borderRadius: new BorderRadius.all(Radius.elliptical(100, 50)),
+        ),
+        child: Center(
+          child: (ty == 'mark')
+              ? FaIcon(FontAwesomeIcons.question)
+              : ((ty == 'tele')
+                  ? FaIcon(FontAwesomeIcons.phone)
+                  : FaIcon(FontAwesomeIcons.handPointLeft)),
+        ),
+      ),
     );
   }
 }
